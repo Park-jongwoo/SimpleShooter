b1077041b02f0def491e849420bd8c3840020650
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2025-06-30 01:45:31 +0900

    Pawn

--- Source/SimpleShooterEditor.Target.cs
+++ Source/SimpleShooterEditor.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class SimpleShooterEditorTarget : TargetRules
 {
-	public SimpleShooterEditorTarget( TargetInfo Target) : base(Target)
+	public SimpleShooterEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
-		ExtraModuleNames.Add("SimpleShooter");
+
+		ExtraModuleNames.AddRange( new string[] { "SimpleShooter" } );
 	}
 }
